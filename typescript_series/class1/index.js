77ab5959ac0bc82f2c122b4490b4af8e9583bfd0
MaryamRiaz-chattha <maryamriaz800@gmail.com>
2024-09-04 18:42:17 +0500

    1st commit

---
+++ typescript_series/class1/index.js
@@ -0,0 +1,28 @@
+"use strict";
+console.log("hello World");
+//console.log 
+//tsc to create index.js
+//rules to declare variables
+//let my name
+let variable = 1234;
+let first_name = "minahil";
+console.log(first_name);
+let mynum$;
+//understanding let /var /const
+//let //keywords
+//let myName="maryam riaz";
+//console.log(myName);
+//let 
+let last_name = "iqra";
+last_name = "ali";
+//last_name="Amir";
+console.log(last_name);
+//var javascript 
+//const constant , value is not changed
+//const my_name="maryam";
+//my_name="ali";
+//type annotation 
+let myname = "mudasir";
+//myname=1234;
+myname = "mubasher";
+console.log(myname);
---
+++ typescript_series/class1/index.ts
@@ -0,0 +1,36 @@
+console.log("hello World");
+//console.log 
+// tsc --init
+//tsc to create index.js
+//rules to declare variables
+//let my name
+let variable=1234;
+let first_name ="minahil";
+console.log(first_name);
+
+let mynum=23
+//understanding let /var /const
+//let //keywords
+//let myName="maryam riaz";
+//console.log(myName);
+//let 
+// keyword variablename = "";//string
+//keyword variablename = ; //numbers
+let classname= 5;
+classname=4;
+
+
+let last_name="iqra";
+last_name="ali";
+//last_name="Amir";
+console.log(last_name);
+//var javascript 
+//const constant , value is not changed
+const my_name="maryam";
+my_name="ali";
+//type annotation 
+let myname:string ="mudasir";
+
+//myname=1234;
+myname="mubasher";
+console.log(myname);
