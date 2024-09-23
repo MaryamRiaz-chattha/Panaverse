9c42d04af91a65101a44bcbee13d024f453b027f
MaryamRiaz-chattha <maryamriaz800@gmail.com>
2024-09-23 19:55:55 +0500

    next js

---
+++ typescript_series/class2/index.js
@@ -0,0 +1,35 @@
+"use strict";
+//Number
+let num = 2;
+console.log(num);
+let num2 = 46788888;
+console.log(num2);
+//string
+let person = "maryam";
+console.log(person);
+let character = "word";
+console.log(character);
+//Boolean data type
+let isSet = true;
+console.log(isSet);
+//undefined
+let unassigned;
+//
+let a; // explicitly declared as undefined
+let b; // implicitly undefined
+console.log(a); // Output: undefined
+console.log(b); // Output: undefined
+//fine way (explicit)
+let mynum = 2; //implicit
+let mynum1 = 2; //explicit
+let variable = null;
+let sentence = "my name is maryam ";
+console.log(sentence);
+let word = "ali";
+let person1 = "ahmad";
+let person2 = `welcome ,${word},${person1}`;
+console.log(person2);
+//template literal
+let stdName = "maarij";
+let stdAge = 30;
+console.log(`Student name is ${stdName}, and the age is ${stdAge}`);
---
+++ typescript_series/class2/index.ts
@@ -0,0 +1,42 @@
+/*Data types in typescript */
+//Number
+let num=2;
+console.log(num);
+let num2=46788888;
+console.log(num2);
+//string
+let person="maryam";
+console.log(person);
+let character="word";
+console.log(character);
+//Boolean data type
+let isSet=true;
+console.log(isSet);
+//undefined
+let unassigned;
+//
+let a: undefined; // explicitly declared as undefined
+let b;            // implicitly undefined
+console.log(a);   // Output: undefined
+console.log(b);   // Output: undefined
+//fine way (explicit)
+let mynum=2;//implicit
+let mynum1: number=2;//explicit
+let variable=null;
+let sentence="my name is maryam "
+console.log(sentence);
+
+//template literal in typescript
+let word:string ="ali";
+let person1:string="ahmad";
+let person2:string=`welcome ,${word},${person1}`;
+console.log(person2);
+//template literal
+let stdName:string="maarij"
+let stdAge:number=30;
+console.log(`Student name is ${stdName}, and the age is ${stdAge}`)
+
+
+
+
+
